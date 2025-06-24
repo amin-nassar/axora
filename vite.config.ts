47dd4a5e36c5068e33e0a9fdda99dc5859bf5b00
amin-nassar <amin.nassar.ce@gmail.com>
2025-06-24 03:06:30 +0300

    Clean up

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"), // Allows you to do `@/components/Button`
+    },
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "Axora",
+      fileName: (format) => `axora.${format}.js`,
+    },
+    rollupOptions: {
+      // Exclude peer dependencies from the bundle
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
